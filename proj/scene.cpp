e5d43d84d7606ba2b3bce897c1fab77f0a8b9c26
darraghhs <162991833+darraghhs@users.noreply.github.com>
2024-12-28 19:26:41 +0000

    Added working infinite terrain, added shaders and struct for skybox

--- proj/scene.cpp
+++ proj/scene.cpp
@@ -38,6 +38,7 @@ float randomFloat();
 int randomInt(int a, int b);
 int randomFloatRange(float a, float b);
 
+static GLuint LoadTerrainTexture(const char *texture_file_path);
 static GLuint LoadTextureTileBox(const char *texture_file_path);
 
 struct AxisXYZ {
@@ -180,6 +181,278 @@ struct Plane {
 
 };
 
+struct Building {
+	glm::vec3 position;		// Position of the box 
+	glm::vec3 scale;		// Size of the box in each axis
+	
+	GLfloat vertex_buffer_data[72] = {	// Vertex definition for a canonical box
+		// Front face
+		-1.0f, -1.0f, 1.0f, 
+		1.0f, -1.0f, 1.0f, 
+		1.0f, 1.0f, 1.0f, 
+		-1.0f, 1.0f, 1.0f, 
+		
+		// Back face 
+		1.0f, -1.0f, -1.0f, 
+		-1.0f, -1.0f, -1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+		1.0f, 1.0f, -1.0f,
+		
+		// Left face
+		-1.0f, -1.0f, -1.0f, 
+		-1.0f, -1.0f, 1.0f, 
+		-1.0f, 1.0f, 1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+
+		// Right face 
+		1.0f, -1.0f, 1.0f, 
+		1.0f, -1.0f, -1.0f, 
+		1.0f, 1.0f, -1.0f, 
+		1.0f, 1.0f, 1.0f,
+
+		// Top face
+		-1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f, -1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+
+		// Bottom face
+		-1.0f, -1.0f, -1.0f, 
+		1.0f, -1.0f, -1.0f, 
+		1.0f, -1.0f, 1.0f, 
+		-1.0f, -1.0f, 1.0f, 
+	};
+
+	GLfloat color_buffer_data[72] = {
+		// Front, red
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		// Back, yellow
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+
+		// Left, green
+		0.0f, 1.0f, 0.0f, 
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		// Right, cyan
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+
+		// Top, blue
+		0.0f, 0.0f, 1.0f, 
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+
+		// Bottom, magenta
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 
+		1.0f, 0.0f, 1.0f, 
+		1.0f, 0.0f, 1.0f,  
+	};
+
+	GLuint index_buffer_data[36] = {		// 12 triangle faces of a box
+		0, 1, 2, 	
+		0, 2, 3, 
+		
+		4, 5, 6, 
+		4, 6, 7, 
+
+		8, 9, 10, 
+		8, 10, 11, 
+
+		12, 13, 14, 
+		12, 14, 15, 
+
+		16, 17, 18, 
+		16, 18, 19, 
+
+		20, 21, 22, 
+		20, 22, 23, 
+	};
+
+    // TODO: Define UV buffer data
+    // ---------------------------
+	GLfloat uv_buffer_data[48]={
+
+		// Front
+		1.0f, 0.665f,
+		0.75f, 0.665f,
+		0.75f, 0.334f,
+		1.0f, 0.334f,
+
+		// Back
+ 		0.5f, 0.665f,
+		0.25f, 0.665f,
+		0.25f, 0.334f,
+		0.5f, 0.334f,
+
+		// Left
+        0.25f, 0.665f,
+		0.0f, 0.665f,
+		0.0f, 0.334f,
+		0.25f, 0.334f,		
+
+		// Right
+        0.75f, 0.665f,
+		0.5f, 0.665f,
+		0.5f, 0.334f,
+		0.75f, 0.334f,
+
+		// Top - we do not want texture the top
+		0.251f, 0.0f,
+		0.499f, 0.0f,
+		0.499f, 0.334f,
+		0.251f, 0.334f,		
+
+		// Bottom - we do not want texture the bottom
+		0.251f, 0.665f,
+		0.499f, 0.665f,
+		0.499f, 1.0f,
+		0.251f, 1.0f,
+	};
+    // ---------------------------
+    
+	// OpenGL buffers
+	GLuint vertexArrayID; 
+	GLuint vertexBufferID; 
+	GLuint indexBufferID; 
+	GLuint colorBufferID;
+	GLuint uvBufferID;
+	GLuint textureID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	GLuint textureSamplerID;
+	GLuint programID;
+
+	void initialize(glm::vec3 position, glm::vec3 scale) {
+		// Define scale of the building geometry
+		this->position = position;
+		this->scale = scale;
+
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		// Create a vertex buffer object to store the vertex data		
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the color data
+        // TODO: 
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+		// TODO: Create a vertex buffer object to store the UV data
+		// --------------------------------------------------------
+		glGenBuffers(1, &uvBufferID);
+ 		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data, GL_STATIC_DRAW);
+        // --------------------------------------------------------
+
+		// Create an index buffer object to store the index data that defines triangle faces
+		glGenBuffers(1, &indexBufferID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_buffer_data), index_buffer_data, GL_STATIC_DRAW);
+
+		// Create and compile our GLSL program from the shaders
+		programID = LoadShadersFromFile("../proj/box.vert", "../lab2/box.frag");
+		if (programID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(programID, "MVP");
+
+        // TODO: Load a texture 
+        // --------------------
+
+		textureID = LoadTextureTileBox("../lab2/sky_debug.png");
+
+        // --------------------
+
+        // TODO: Get a handle to texture sampler 
+        // -------------------------------------
+		textureSamplerID = glGetUniformLocation(programID,"textureSampler");
+        // -------------------------------------
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(programID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+
+		// TODO: Model transform 
+		// -----------------------
+        glm::mat4 modelMatrix = glm::mat4();    
+		modelMatrix = glm::translate(modelMatrix, position);
+        // Scale the box along each axis to make it look like a building
+        modelMatrix = glm::scale(modelMatrix, scale);
+        // -----------------------
+
+		// Set model-view-projection matrix
+		glm::mat4 mvp = cameraMatrix * modelMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+		// TODO: Enable UV buffer and texture sampler
+		// ------------------------------------------
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		// Set textureSampler to use texture unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glUniform1i(textureSamplerID, 0); 
+
+        // ------------------------------------------
+
+		// Draw the box
+		glDrawElements(
+			GL_TRIANGLES,      // mode
+			36,    			   // number of indices
+			GL_UNSIGNED_INT,   // type
+			(void*)0           // element array buffer offset
+		);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+        //glDisableVertexAttribArray(2);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteBuffers(1, &indexBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		//glDeleteBuffers(1, &uvBufferID);
+		//glDeleteTextures(1, &textureID);
+		glDeleteProgram(programID);
+	}
+}; 
+
 struct Terrain {
     int m_terrainSize = 0;
     int m_width = 0;
@@ -249,7 +522,7 @@ struct Terrain {
 
         printf("1. Shaders loaded\n");
 
-        this->textureID = LoadTextureTileBox("../proj/textures/texture4.jpg");
+        this->textureID = LoadTerrainTexture("../proj/textures/texture4.jpg");
 
         this->mvpMatrixID = glGetUniformLocation(this->programID, "MVP");
         this->minID = glGetUniformLocation(this->programID, "minHeight");
@@ -279,7 +552,7 @@ struct Terrain {
 
         this->init(offset);
 
-        CreateMidpointDisplacement(this, 128, 1, 1, 50);
+        CreateMidpointDisplacement(this, 1024, 1, 1, 350);
 
         this->populateBuffers(0);
     }
@@ -580,8 +853,6 @@ struct Terrain {
 
 };
 
-void resetEye(Terrain* terrain);
-
 
 int initWindow(){
 
@@ -639,10 +910,14 @@ void initCamera(glm::mat4 &projectionMatrix){
     // Creating projection matrix
     glm::float32 FoV = 60;
 	glm::float32 zNear = 0.1f; 
-	glm::float32 zFar = 4000.0f;
+	glm::float32 zFar = 1000.0f;
 	projectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, zNear, zFar);
 
 }
+
+
+
+void resetEye(Terrain* terrain);
 
 Array2D<float> flipHeightMapX(Array2D<float>& heightMap, float terrainSize);
 Array2D<float> flipHeightMapZ(Array2D<float>& heightMap, float terrainSize);
@@ -656,7 +931,7 @@ int main(void){
     }
 
     // Setting background colour
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.5f, 0.7f, 0.0f);
     glFrontFace(GL_CW);
     glCullFace(GL_FRONT);
     glEnable(GL_DEPTH_TEST);
@@ -768,9 +1043,9 @@ int main(void){
 
     do{
 
-        //resetEye(&terrain);
+        resetEye(&terrain);
 
-        light += 0.0002f;
+        light += 0.001f;
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -984,7 +1259,7 @@ int randomFloatRange(float a, float b){
     return(float)randomInt(a, b) + randomFloat();
 }
 
-static GLuint LoadTextureTileBox(const char *texture_file_path) {
+static GLuint LoadTerrainTexture(const char *texture_file_path) {
     int w, h, channels;
     uint8_t* img = stbi_load(texture_file_path, &w, &h, &channels, 3);
     GLuint texture;
@@ -993,7 +1268,6 @@ static GLuint LoadTextureTileBox(const char *texture_file_path) {
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);	
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
@@ -1077,6 +1351,30 @@ Array2D<float> copyHeightMap(Array2D<float>& heightMap, float terrainSize){
 
     return out;
 
+}
+
+static GLuint LoadTextureTileBox(const char *texture_file_path) {
+    int w, h, channels;
+    uint8_t* img = stbi_load(texture_file_path, &w, &h, &channels, 3);
+    GLuint texture;
+    glGenTextures(1, &texture);  
+    glBindTexture(GL_TEXTURE_2D, texture);  
+
+    // To tile textures on a box, we set wrapping to repeat
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    if (img) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture " << texture_file_path << std::endl;
+    }
+    stbi_image_free(img);
+
+    return texture;
 }
 
 
